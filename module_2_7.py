1b860faaca2ca2ee938bd0088b10173893d172a9
andreysavart <andrejsavkin751@gmail.com>
2024-06-11 22:15:26 +0300

    Add files via upload

---
+++ module_2_7.py
@@ -0,0 +1,17 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+print_params(10)
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+values_list = [10, 'строковое значение', False]
+values_dict = {'a': 100, 'b': 'новая строка', 'c': True}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+
+print_params(*values_list_2, 42)
